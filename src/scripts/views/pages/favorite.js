a2cc9297be5454ec9dee1be81f329561aa897a8c
Titip Barkah <tibar.cbs@gmail.com>
2024-12-24 23:04:53 +0700

    perapihan-tampilan-hero

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -5,23 +5,26 @@ const Favorite = {
     async render() {
         return `
         <section class="hero" id="hero">
+            <picture>
+                <source media="(max-width: 500px)" srcset="./public/images/heros/hero-image_2-small.webp">
+                <source media="(max-width: 800px)" srcset="./public/images/heros/hero-image_2-medium.webp">
+                <img src="./public/images/heros/hero-image_2-large.webp" alt="">
+            </picture>
             <div class="content">
                 <h3>
-                Nikmati Setiap <span>Gigitan</span> , Rasakan <span>Kebahagiaan</span>
+                    Nikmati Setiap <span>Gigitan</span> , Rasakan <span>Kebahagiaan</span>
                 </h3>
                 <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-                reiciendis ipsam fugiat praesentium, porro accusantium tempora commodi
-                ipsa. Consequuntur illo corrupti ipsam obcaecati voluptas quos sunt
-                omnis quo aperiam corporis.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sunt fugit ipsum ea voluptates mollitia, odit vel. 
+                    Fugit magnam, saepe rerum iusto cum quia illum quis quod consequuntur, delectus ratione!
                 </p>
             </div>
-            </section>
-                <section class="menu" id="menu">
-                <h1 class="heading">Favorite <span>Restorant</span></h1>
-                <div class="box-container"></div>
-            </section>
-            <div class="loader" aria-label="Sedang Memuat Data"></div>
+        </section>
+        <section class="menu" id="menu">
+            <h1 class="heading">Favorite <span>Restorant</span></h1>
+            <div class="box-container"></div>
+        </section>
+        <div class="loader" aria-label="Sedang Memuat Data"></div>
       `;
     },
     _hideLoading() {
